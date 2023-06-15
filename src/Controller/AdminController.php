d59088f7b743caeaf034ae9a0b128e7d39140b8d
FranBo10-Doranco <franbo10pro@gmail.com>
2023-06-15 17:00:35 +0200

    Affichage voitures et membres

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Entity\Vehicule;
+use App\Form\CommandeType;
 use App\Form\VehiculeType;
+use App\Repository\UserRepository;
+use App\Repository\CommandeRepository;
 use App\Repository\VehiculeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,65 +18,127 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
-    {
-        return $this->render('admin/index.html.twig');
-    }
-
-    #[Route('/admin/gestion', name: 'admin_gestion')]
-    public function gestion(VehiculeRepository $repo) :Response
-    {
-        $vehicules = $repo->findAll();
-        return $this->render('admin/gestion.html.twig', [
-            'vehicules' => $vehicules
-        ]);
-    }
-
-    #[Route('/admin/add', name: 'admin_add')]
+    #[Route('/admin/vehicules', name: 'admin_vehicules')]
     #[Route('/admin/edit/{id}', name: 'admin_edit')]
-    public function add(Request $request, EntityManagerInterface $manager, Vehicule $vehicule = null) :Response {
+    public function gestion(Request $request, EntityManagerInterface $manager, VehiculeRepository $repo, Vehicule $vehicule = null) :Response
+    {     
+        $voitures = $repo->findAll();
 
         if($vehicule == null) 
         {
             $vehicule = new vehicule;        
         }
-
+        
         $form = $this->createForm(VehiculeType::class, $vehicule);
         $form->handleRequest($request);
         
-        $modele = $request->request->get('modele');
         if($form->isSubmitted() && $form->isValid()) {
-            $marque = $request->request->get('marque');
             $vehicule->setTitre($vehicule->getMarque() . " " . $vehicule->getModele());
             $vehicule->setDateEnregistrement(new \DateTime);
             $manager->persist($vehicule);
             $manager->flush();
             $this->addFlash('success', 'Les données ont été bien enregistrées');
-            return $this->redirectToRoute('admin_gestion');
+            return $this->redirectToRoute('admin_vehicules');
         }
-
-        return $this->render('admin/formVehicule.html.twig', [
+        
+        return $this->render('admin/index.html.twig', [
+            'vehicules' => $voitures,
             'form' => $form,
-            'editMode' => $vehicule->getId()
+            'editMode' => $vehicule->getId() != null,
         ]);
     }
 
+    
     #[Route('/admin/delete/{id}', name: 'admin_delete')]
     public function delete(EntityManagerInterface $manager, Vehicule $vehicule) {
         $manager->remove($vehicule);
         $manager->flush();
         $this->addFlash('warning', 'Les données ont été bien suprimées');
-        return $this->redirectToRoute('admin_gestion');
+        return $this->redirectToRoute('admin_vehicules');
         
     }    
 
-    #[Route('/app/show/{id}', name: 'app_show')]
-    public function show(VehiculeType $repo, EntityManagerInterface $manager, Vehicule $vehicule) :Response
-    {
-        $vehicules = $repo->find(['id' => $vehicule->getId()]);
-        $repo->handleRequest();
+    #[Route('/admin/show/{id}', name: 'admin_show')]
+    public function show(Vehicule $vehicule) :Response
+    {   
+        return $this->render('admin/show.html.twig', [
+            'vehicule' => $vehicule
+        ]);
+    }
 
+    #[Route('admin/users', name: 'admin_users')]
+    public function user(Request $request, EntityManagerInterface $manager, UserRepository $repo, User $user = null) {
+        $users = $repo->findAll();
+
+        if($user == null) {
+            $user = new User;
+        }
+
+        $formUser = $this->createForm(UserType::class, $user);
+        $formUser->handleRequest($request);
+        if($formUser->isSubmitted() && $formUser->isValid()) {
+            $user->setDateEnregistrement(new \DateTime);
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash('success', 'Utilisateur enregistré');
+            return $this->redirectToRoute('admin_users');
+        }
+
+        return $this->render('admin/gestionUser.html.twig', [
+            'users' => $users,
+            'form' => $formUser,
+            'editMode' => $user->getId() != null,
+        ]);
+
+        return $this->render("admin/gestionUser.html.twig", [
+            'users' => $users
+        ]);
+    }
+
+    #[Route('admin/user/edit{id}', name: 'admin_user_edit')]
+    public function userEdit(User $user) 
+    {
+        if(in_array("ROLE_ADMIN", $user->getRoles())) {
+            $role = "ROLE_ADMIN";
+        } 
+        return $this->render ('admin/modifyUser.html.twig',['role' => $role] ) ;
+    }
+
+    #[Route('admin/buy', name: 'admin_buy')]
+    public function buy(Request $request, EntityManagerInterface $manager, CommandeRepository $repo, Commande $commande = null, Vehicule $vehicule, User $user) {
+        $commandes = $repo->findAll();
+
+        if ($commande == null) {
+            $commande = new commande;
+        }
+
+        $formCommande = $this->createForm(CommandeType::class, $commande);
+        $formCommande->handleRequest($request);
+
+        $dateDepart = $commande->getDateDepart();
+        $dateFin = $commande->getDateHeureFin();
+        $prix = $vehicule->getPrixJournalier();
+        $prixTotal = ($dateDepart - $dateFin) * $prix;
+
+        if($formCommande->isSubmitted() && $formCommande->isValid()) {
+            $commande->setDateEnregistrement(new \DateTime)
+                    ->setPrixTotal($prixTotal)
+                    ->setUser($user)
+                    ->setVehicule($this->getId() . " - " . $this->getTitre());
+        }
+
+
+
+        return $this->render('admin/gestionCommandes.html.twig');
 
     }
+
+
+
+
+
+
+
+
+
 }
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -9,21 +9,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AppController extends AbstractController
 {
-    #[Route('/app', name: 'app_app')]
-    public function index(): Response
-    {
-        return $this->render('app/index.html.twig');
-    }
-
-    #[Route('/app/show', name: 'app_show')]
-    public function show(VehiculeRepository $repo) :Response 
+    #[Route('/', name: 'home')]
+    public function index(VehiculeRepository $repo): Response
     {
         $vehicules = $repo->findAll();
 
-        return $this->render('app/show.html.twig', [
+        return $this->render('app/index.html.twig', [
             'vehicules' => $vehicules
         ]);
-
-
     }
+
+   
 }
